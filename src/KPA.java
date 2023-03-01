a1f04628f8cb70a057ab3efb50e6452f35d2c2c3
Petros Kitazos <petroskitazos@gmail.com>
2023-03-01 18:01:20 +0000

    refactored code to remove unused helpers and contain everything in just 2 classes

--- src/KPA.java
+++ src/KPA.java
@@ -5,7 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 
-public class KPAv2 {
+public class KPA {
 
     public static void main(String[] args) {
 
@@ -14,17 +14,16 @@ public class KPAv2 {
         String ct = "g5P\u007FWC9WwvV2./gvu\\5fV%D}]^RQ^Cc>u$zoj2l`AIhZD.Q1Gl?K,BF'U8rw_^(mVzJk:LmLmx(|P qFjMi\"_gQfm6#84r;1,u;I";
         String known_pt = "Th";
 
-
         ArrayList<String> good_keys = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(f))) {
             String key;
             while ((key = br.readLine()) != null) {
-                String encrypted_known_pt = Rotor96Crypto.encdec(1, key, known_pt);
-                if (encrypted_known_pt.equals(ct.substring(0, 2))) {
+                String decrypted_ct = Rotor96Crypto.encdec(2, key, ct);
+                if (known_pt.equals(decrypted_ct.substring(0, 2))) {
                     good_keys.add(key);
                 }
             }
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -33,6 +32,7 @@ public class KPAv2 {
         for (String key : good_keys) {
             System.out.println(Rotor96Crypto.encdec(2, key, ct));
         }
+
     }
 }
 
